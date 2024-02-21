17c212403a2f04ec3c73e2add581dde292b84df6
matheus-modesto <matheusmodestto@gmail.com>
2024-02-21 12:02:44 -0300

    img changes

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 const convertbutton = document.querySelector(".convert-button")
 const currencyselect = document.querySelector(".currency-select")
 
+
 function convertvalues() {
     const inputcurrencyvalue = document.querySelector(".currency-value").value
     const currencyvalue = document.querySelector(".currency-value-to-convert")
     const currencyvalueconverted = document.querySelector(".currency-value-converted")
-    
-    
-    const dolartoday = 5.2
+   
+     const dolartoday = 5.2
     const eurotoday = 6.2
     
+    console.log(currencyselect.value)
 
     if(currencyselect.value== "dolar"){
         currencyvalueconverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -39,6 +40,20 @@ function convertvalues() {
 
 
 }
+function changecurrency(){
+    const currencyname= document.getElementById("currency-name")
+    const currencyimg= document.querySelector(".currency-img")
+    if(currencyselect.value== "dolar"){
+        currencyname.innerHTML = "Dolar Americano"
+        currencyimg.src="./assets/dolar.png"
+    }
+    if(currencyselect.value== "euro"){
+        currencyname.innerHTML= "euro"
+        currencyimg.src="./assets/euro.png"
 
+    }
+}
 
+currencyselect.addEventListener("change",changecurrency)
 convertbutton.addEventListener("click", convertvalues)
+
